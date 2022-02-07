d403f9ba636a67cb4cd0c58e543409e7234deb22
Przemysław Markiewicz <przemyslawmarkiewicz97@gmail.com>
2022-02-07 15:50:14 +0100

    Updated setup and requirements

--- setup.py
+++ setup.py
@@ -7,8 +7,13 @@ name = 'audoma'
 description = 'API Automatic Documentation Maker - YASG/SPECTACULAR wrapper'
 
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+
 setup(
     name=name,
     version='1.0.0',
-    packages=find_packages('')
+    packages=find_packages(''),
+    install_requires=required
 )
