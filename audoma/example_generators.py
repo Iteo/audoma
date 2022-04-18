33b54360aec7cc15452c7d58f7934d8b93025b09
Przemysław Markiewicz <przemyslawmarkiewicz97@gmail.com>
2022-04-18 12:55:57 +0200

    PINT-26-charfield-example - fixed lorem ipsum genertor

--- audoma/example_generators.py
+++ audoma/example_generators.py
@@ -5,8 +5,10 @@ import lorem
 
 def generate_lorem_ipsum(min_length=20, max_length=80):
     random_lorem = lorem.text()
-    max_length = max([min([max_length, 80]), max_length])
-    min_length = max([min_length, 20])
-    if min_length > max_length:
-        min_length = max_length
+    if max_length < 20:
+        max_length = max_length
+    else:
+        max_length = max([min([80, max_length]), min_length])
+    min_length = min([max([20, min_length]), max_length])
+
     return random_lorem[: random.randint(min_length, max_length)]
