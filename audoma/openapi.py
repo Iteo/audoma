86e0771999664f628bdbf9cefbb5df0a7768faa6
mtyton <mateusz.tyton@iteo.com>
2022-05-31 15:02:12 +0200

    PINT-42: moved some stuff from openapi_helpersto AudomaAutoSchema

--- audoma/openapi.py
+++ audoma/openapi.py
@@ -1,6 +1,6 @@
-from __future__ import annotations
-
 import typing
+from copy import deepcopy
+from inspect import isclass
 
 from drf_spectacular.drainage import get_override
 from drf_spectacular.extensions import OpenApiSerializerExtension
@@ -12,29 +12,196 @@ from drf_spectacular.plumbing import (
     sanitize_specification_extensions,
 )
 from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import OpenApiResponse
 from rest_framework.fields import Field
 from rest_framework.generics import GenericAPIView
+from rest_framework.permissions import (
+    AND,
+    OR,
+    BasePermission,
+    OperandHolder,
+    SingleOperandHolder,
+)
 from rest_framework.serializers import BaseSerializer
 from rest_framework.views import APIView
+
+from django.views import View
 
 from audoma.drf.generics import GenericAPIView as AudomaGenericAPIView
 from audoma.drf.validators import ExclusiveFieldsValidator
 from audoma.openapi_helpers import (
-    AudomaApiResponseCreator,
     build_exclusive_fields_examples,
     build_exclusive_fields_schema,
-    get_permissions_description,
 )
 
 
 class AudomaAutoSchema(AutoSchema):
-    response_creator = AudomaApiResponseCreator()
+    def _handle_permission(
+        self,
+        permission_class: typing.Union[
+            OperandHolder, SingleOperandHolder, BasePermission
+        ],
+        operations: list,
+        current_operation: typing.Type = AND,
+    ) -> dict:
+        permissions = {}
+
+        if isinstance(permission_class, OperandHolder):
+            if permission_class.operator_class == OR and current_operation != OR:
+                operations.append("(")
+            permissions.update(
+                self._handle_permission(
+                    permission_class.op1_class,
+                    operations,
+                    permission_class.operator_class,
+                )
+            )
+            if permission_class.operator_class == OR:
+                operations.append("|")
+            elif permission_class.operator_class == AND:
+                operations.append(" & ")
+            permissions.update(
+                self._handle_permission(
+                    permission_class.op2_class,
+                    operations,
+                    permission_class.operator_class,
+                )
+            )
+            if permission_class.operator_class == OR and current_operation != OR:
+                operations.append(" )")
+        elif isinstance(permission_class, SingleOperandHolder):
+            permissions.update(
+                self._handle_permission(
+                    permission_class.op1_class,
+                    operations,
+                    permission_class.operator_class,
+                )
+            )
+
+        else:
+            try:
+                permissions[permission_class.__name__] = (
+                    permission_class.get_description(self.view),
+                )
+            except AttributeError:
+                if permission_class.__doc__:
+                    permissions[
+                        permission_class.__name__
+                    ] = permission_class.__doc__.replace("\n", " ").strip()
+                else:
+                    permissions[
+                        permission_class.__name__
+                    ] = "(No description for this permission)"
+            operations.append(f"`{permission_class.__name__}`")
+
+        return permissions
+
+    def _get_permissions_description(self) -> str:
+        permissions = {}
+        operations = []
+
+        for permission_class in getattr(self.view, "permission_classes", []):
+            if operations:
+                operations.append("&")
+            permissions.update(self._handle_permission(permission_class, operations))
+
+        if permissions:
+            return (
+                "\n\n**Permissions:**\n"
+                + " ".join(operations)
+                + "\n"
+                + "\n".join(
+                    f"+ `{name}`: *{doc_str}*" for name, doc_str in permissions.items()
+                )
+            )
+        else:
+            return ""
 
     def get_description(self) -> str:
-        view = self.view
         description = super().get_description() or ""
-        description += get_permissions_description(view)
+        description += self._get_permissions_description()
         return description
+
+    def _extract_action_function(self, view) -> typing.Callable:
+        action = getattr(view, "action", None)
+        if not action:
+            return
+
+        return getattr(view, action, None)
+
+    def _parse_action_serializers(self, action_serializers) -> dict:
+        if not action_serializers:
+            return action_serializers
+
+        if isinstance(action_serializers, str):
+            return {"default": OpenApiResponse(description=action_serializers)}
+
+        if not isinstance(action_serializers, dict):
+            return {"default": action_serializers}
+
+        parsed_action_serializers = deepcopy(action_serializers)
+
+        for method, method_serializers in action_serializers.items():
+            if isinstance(method_serializers, str):
+                parsed_action_serializers[method] = OpenApiResponse(
+                    description=method_serializers
+                )
+            elif isinstance(method_serializers, dict):
+                for code, item in method_serializers.items():
+                    if isinstance(item, str):
+                        parsed_action_serializers[method][code] = OpenApiResponse(
+                            description=item
+                        )
+
+        return parsed_action_serializers
+
+    def _parse_action_errors(self, action_errors) -> dict:
+        if not action_errors:
+            return action_errors
+
+        parsed_errors = {}
+        for err in action_errors:
+            if isclass(err):
+                err = err()
+
+            # build properties
+            properties = {}
+            # TODO - stop using __dict__ here, use something else
+            for key, value in err.__dict__.items():
+                properties[key] = {key: {"type": type(value).__name__}}
+
+            parsed_errors[err.status_code] = OpenApiResponse(
+                response={
+                    "type": "object",
+                    "properties": properties,
+                    "example": err.__dict__,
+                }
+            )
+        return parsed_errors
+
+    def _extract_audoma_action_operations(
+        self, view: View, serializer_type: str
+    ) -> dict:
+        """
+        Extracts the audoma action operations from the view
+        """
+        action_function = self._extract_action_function(view)
+        _audoma = getattr(action_function, "_audoma", None)
+        if not _audoma:
+            return {}
+
+        if serializer_type == "collect":
+            action_serializers = getattr(_audoma, "collectors", None)
+        else:
+            results = self._parse_action_serializers(getattr(_audoma, "results", None))
+            errors = self._parse_action_errors(getattr(_audoma, "errors", {}))
+            if results:
+                action_serializers = results
+                action_serializers.update(errors)
+            else:
+                action_serializers = errors
+
+        return action_serializers
 
     def _get_serializer(  # noqa: C901
         self, serializer_type="collect"
@@ -42,11 +209,9 @@ class AudomaAutoSchema(AutoSchema):
         view = self.view
         method = view.request.method
 
-        # code responsible for documenting AudomaAction decorator
-        if serializer_type == "collect":
-            action_serializers = self.response_creator.extract_collectors(view)
-        else:
-            action_serializers = self.response_creator.extract_results(view)
+        action_serializers = self._extract_audoma_action_operations(
+            view, serializer_type
+        )
 
         if action_serializers:
             if (
