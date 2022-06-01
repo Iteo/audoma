4168367154c207c55a07e8add7567c8312df0bce
Przemysław Markiewicz <przemyslawmarkiewicz97@gmail.com>
2022-06-01 08:31:30 +0200

    pint-42-code-refactor - moved ExampleModelMixin to audoma mixins

--- audoma/mixins.py
+++ audoma/mixins.py
@@ -40,3 +40,10 @@ class NumericExampleMixin(ExampleMixin):
 
 class RegexExampleMixin(ExampleMixin):
     audoma_example_class = RegexExample
+
+
+class ModelExampleMixin:
+    def __init__(self, *args, **kwargs) -> None:
+        if kwargs.get("example", None):
+            self.example = kwargs.pop("example", None)
+        super().__init__(*args, **kwargs)
