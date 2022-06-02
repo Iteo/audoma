71713608944234f4bec8f30fbc6cd8e0d489aad4
Przemysław Markiewicz <przemyslawmarkiewicz97@gmail.com>
2022-06-02 08:59:29 +0200

    PINT-40-add-docstrings - added basic documentation configuration

--- audoma/drf/fields.py
+++ audoma/drf/fields.py
@@ -1,10 +1,3 @@
-"""Audoma DRF Fields
-This module contains all the fields from Django Rest Framework with additional functionality.
-By inheriting from Audoma Mixins, an example is generated for each field (i.e. FloatField will have
-example generated based on field's min and max values).
-We can define custom example by simply passing `example` as an argument to the field.
-"""
-
 import sys
 
 import exrex
@@ -54,11 +47,11 @@ this = sys.modules[__name__]
 
 for field_name in field_names:
 
-    class Field(ExampleMixin, getattr(fields, field_name)):
-        pass
-
-    Field.__name__ = field_name
-    setattr(this, field_name, Field)
+    setattr(
+        this,
+        field_name,
+        type(field_name, (ExampleMixin, getattr(fields, field_name)), {}),
+    )
 
 
 class DecimalField(NumericExampleMixin, fields.DecimalField):
---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+.. audoma - API Automatic Documentation Maker documentation master file, created by
+   sphinx-quickstart on Wed Jun  1 08:52:59 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to audoma - API Automatic Documentation Maker's documentation!
+======================================================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
