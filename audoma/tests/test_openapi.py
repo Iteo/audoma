db532536195670117b08c4f6ed007bf82ef2a5eb
mtyton <mateusz.tyton@iteo.com>
2022-07-23 07:36:46 +0200

    PINT-50: added more tests

--- audoma/tests/test_openapi.py
+++ audoma/tests/test_openapi.py
@@ -1,9 +1,12 @@
 from unittest import TestCase
 
+from rest_framework import fields
 from rest_framework.permissions import (
     BasePermission,
     IsAuthenticated,
 )
+from rest_framework.serializers import Serializer
+from rest_framework.test import APIRequestFactory
 
 from audoma.drf.mixins import (
     ActionModelMixin,
@@ -17,33 +20,27 @@ from audoma.drf.mixins import (
 )
 from audoma.drf.viewsets import GenericViewSet
 from audoma.openapi import AudomaAutoSchema
+from audoma.tests.testtools import (
+    create_serializer,
+    create_serializer_class,
+    create_view,
+)
 
 
 class AudomaAutoSchemaTestCase(TestCase):
     def setUp(self) -> None:
         super().setUp()
+        self.factory = APIRequestFactory()
 
     def test_get_description_no_permissions_success(self):
-        class ExampleView(ListModelMixin, RetrieveModelMixin, GenericViewSet):
-            """
-            Test View Doc
-            """
-
-            permission_classes = []
-
-        view = ExampleView()
+        view = create_view(view_properties={"permission_classes": []})
         view.schema = AudomaAutoSchema()
         view.schema.method = "get"
         desc = view.schema.get_description()
         self.assertEqual(desc, """Test View Doc""")
 
     def test_get_description_with_basic_permissions_success(self):
-        class ExampleView(ListModelMixin, RetrieveModelMixin, GenericViewSet):
-            """
-            Test View Doc
-            """
-
-        view = ExampleView()
+        view = create_view()
         view.schema = AudomaAutoSchema()
         view.schema.method = "get"
         desc = view.schema.get_description()
@@ -60,16 +57,138 @@ class AudomaAutoSchemaTestCase(TestCase):
             def has_permission(self, requst, view):
                 ...
 
-        class ExampleView(ListModelMixin, RetrieveModelMixin, GenericViewSet):
-            """
-            Test View Doc
-            """
-
-            permission_classes = [ExampleCustomPermission & IsAuthenticated]
-
-        view = ExampleView()
+        permission_classes = [ExampleCustomPermission & IsAuthenticated]
+        view = create_view(view_properties={"permission_classes": permission_classes})
         view.schema = AudomaAutoSchema()
         view.schema.method = "get"
         desc = view.schema.get_description()
         self.assertIn("`ExampleCustomPermission`  &  `IsAuthenticated`", desc)
         self.assertIn("Test View Doc", desc)
+
+    def test_get_description_no_description_permission_success(self):
+        class SomePermission(BasePermission):
+            ...
+
+        permission_classes = [SomePermission]
+        view = create_view(view_properties={"permission_classes": permission_classes})
+        view.schema = AudomaAutoSchema()
+        view.schema.method = "get"
+        desc = view.schema.get_description()
+        self.assertIn("(No description for this permission)", desc)
+
+    def test_get_serializer_default_serializer_usage(self):
+        fields_config = {
+            "name": fields.CharField(max_length=255),
+            "age": fields.IntegerField(),
+        }
+
+        serializer_class = create_serializer_class(
+            fields_config=fields_config, serializer_base_classes=[Serializer]
+        )
+        view = create_view(view_properties={"serializer_class": serializer_class})
+        view.schema = AudomaAutoSchema()
+        view.request = self.factory.get("/example/")
+        view.action = "retrieve"
+        serializer = view.schema._get_serializer()
+        self.assertEqual(serializer_class, type(serializer))
+
+    def test_get_serializer_collect_serializer_class_success(self):
+        fields_config = {
+            "name": fields.CharField(max_length=255),
+            "age": fields.IntegerField(),
+        }
+
+        serializer_class = create_serializer_class(
+            fields_config=fields_config, serializer_base_classes=[Serializer]
+        )
+        view = create_view(
+            view_properties={"common_collect_serializer_class": serializer_class}
+        )
+        view.schema = AudomaAutoSchema()
+        view.request = self.factory.get("/example/")
+        view.action = "retrieve"
+        serializer = view.schema._get_serializer()
+        self.assertEqual(serializer_class, type(serializer))
+
+    def test_get_serializer_result_serializer_class_success(self):
+        fields_config = {
+            "name": fields.CharField(max_length=255),
+            "age": fields.IntegerField(),
+        }
+
+        serializer_class = create_serializer_class(
+            fields_config=fields_config, serializer_base_classes=[Serializer]
+        )
+        view = create_view(
+            view_properties={"common_result_serializer_class": serializer_class}
+        )
+        view.schema = AudomaAutoSchema()
+        view.request = self.factory.get("/example/")
+        view.action = "retrieve"
+        serializer = view.schema._get_serializer(serializer_type="result")
+        self.assertEqual(serializer_class, type(serializer))
+
+    def test_get_serializer_retrieve_serializer_class_success(self):
+        fields_config = {
+            "name": fields.CharField(max_length=255),
+            "age": fields.IntegerField(),
+        }
+
+        serializer_class = create_serializer_class(
+            fields_config=fields_config, serializer_base_classes=[Serializer]
+        )
+        view = create_view(
+            view_properties={"retrieve_serializer_class": serializer_class}
+        )
+        view.schema = AudomaAutoSchema()
+        view.request = self.factory.get("/example/")
+        view.action = "retrieve"
+        serializer = view.schema._get_serializer(serializer_type="result")
+        self.assertEqual(serializer_class, type(serializer))
+
+    def test_get_serializer_get_retrieve_serializer_class_success(self):
+        fields_config = {
+            "name": fields.CharField(max_length=255),
+            "age": fields.IntegerField(),
+        }
+
+        serializer_class = create_serializer_class(
+            fields_config=fields_config, serializer_base_classes=[Serializer]
+        )
+        view = create_view(
+            view_properties={"get_retrieve_serializer_class": serializer_class}
+        )
+        view.schema = AudomaAutoSchema()
+        view.request = self.factory.get("/example/")
+        view.action = "retrieve"
+        serializer = view.schema._get_serializer(serializer_type="result")
+        self.assertEqual(serializer_class, type(serializer))
+
+    def test_get_serializer_get_retrieve_result_serializer_class_success(self):
+        fields_config = {
+            "name": fields.CharField(max_length=255),
+            "age": fields.IntegerField(),
+        }
+
+        serializer_class = create_serializer_class(
+            fields_config=fields_config, serializer_base_classes=[Serializer]
+        )
+        view = create_view(
+            view_properties={"get_retrieve_result_serializer_class": serializer_class}
+        )
+        view.schema = AudomaAutoSchema()
+        view.request = self.factory.get("/example/")
+        view.action = "retrieve"
+        serializer = view.schema._get_serializer(serializer_type="result")
+        self.assertEqual(serializer_class, type(serializer))
+
+    def test_get_serializer_no_serializer_class_failure(self):
+        view = create_view(view_properties={"serializer_class": None})
+        view.schema = AudomaAutoSchema()
+        view.request = self.factory.get("/example/")
+        view.action = "retrieve"
+        serializer = view.schema._get_serializer(serializer_type="result")
+        self.assertIsNone(serializer)
+
+    def test_get_serializer_class_audoma_action_success(self):
+        ...
