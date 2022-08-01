fb93b510d9185ef8bc7f8948e4eabadda0572666
mtyton <mateusz.tyton@iteo.com>
2022-08-01 12:05:20 +0200

    PINT-50: added more tests

--- audoma/tests/test_schema.py
+++ audoma/tests/test_schema.py
@@ -4,6 +4,7 @@ from django_filters.filters import (
 )
 from django_filters.rest_framework import DjangoFilterBackend
 from drf_spectacular.plumbing import ComponentRegistry
+from rest_framework.filters import SearchFilter
 from rest_framework.serializers import Serializer
 from rest_framework.test import APIRequestFactory
 
@@ -18,7 +19,10 @@ from audoma.choices import make_choices
 from audoma.drf import fields as audoma_fields
 from audoma.drf.filters import DocumentedTypedChoiceFilter
 from audoma.openapi import AudomaAutoSchema
-from audoma.schema import AudomaDjangoFilterExtension
+from audoma.schema import (
+    AudomaDjangoFilterExtension,
+    SearchFilterExtension,
+)
 from audoma.tests.testtools import (
     create_basic_view,
     create_model_class,
@@ -95,4 +99,57 @@ class AudomaDjangoFilterExtensionTestCase(TestCase):
         self.assertEqual(
             result[0]["schema"]["x-choices"],
             {"choices": {"LIKE": "Like", "DISLIKIE": "Dislike"}},
+        )
+
+
+class SearchFilterExtensionTestCase(TestCase):
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.choices = make_choices(
+            "RATE", ((0, "LIKE", "Like"), (1, "DISLIKIE", "Dislike"))
+        )
+        self.fields_config = {
+            "company_name": audoma_fields.CharField(max_length=255),
+            "city": audoma_fields.CharField(),
+            "company_rate": audoma_fields.ChoiceField(
+                choices=self.choices.get_choices()
+            ),
+        }
+        serializer_class = create_serializer_class(
+            fields_config=self.fields_config,
+            serializer_base_classes=[Serializer],
+        )
+        request = self.factory.post("/example/")
+        self.model_fields_config = {
+            "company_name": models.CharField(max_length=255),
+            "city": models.CharField(max_length=255),
+            "company_rate": models.CharField(choices=self.choices.get_choices()),
+        }
+        model = create_model_class(fields_config=self.model_fields_config)
+        self.view = create_basic_view(
+            view_properties={"serializer_class": serializer_class}
+        )
+        self.view.request = request
+        self.view.action = "create"
+        self.view.schema = AudomaAutoSchema()
+        self.view.schema.registry = ComponentRegistry()
+        self.view.schema.method = "POST"
+        self.view.model = model
+        self.view.search_fields = ["company_name", "city"]
+        self.extension = SearchFilterExtension(target=SearchFilter())
+
+    def test_serach_filter_generate_default_description(self):
+        result = self.extension.get_schema_operation_parameters(self.view.schema)
+        self.assertEqual(
+            result[0]["description"], "Search by: \n* `company_name` \n* `city` \n"
+        )
+
+    def test_serach_filter_generate_custom_simple_description(self):
+        self.extension.target.search_description = (
+            "Some Custom description.\n {search_fields}"
+        )
+        result = self.extension.get_schema_operation_parameters(self.view.schema)
+        self.assertEqual(
+            result[0]["description"],
+            "Some Custom description.\n Search by: \n* `company_name` \n* `city` \n",
         )
--- audoma_examples/drf_example/drf_example/settings.py
+++ audoma_examples/drf_example/drf_example/settings.py
@@ -159,9 +159,9 @@ PHONENUMBER_DEFAULT_FORMAT = "INTERNATIONAL"
 TEST_RUNNER = "django_nose.NoseTestSuiteRunner"
 
 NOSE_ARGS = (
-    # "--with-coverage",
-    # "--cover-inclusive",
-    # "--cover-package=audoma_api",
-    # "--cover-package=audoma",
-    # "--cover-min-percentage=90",
+    "--with-coverage",
+    "--cover-inclusive",
+    "--cover-package=audoma_api",
+    "--cover-package=audoma",
+    "--cover-min-percentage=90",
 )
