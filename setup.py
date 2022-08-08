24230298d45c5358e3ddd6ca3017500bee762d49
mtyton <mateusz.tyton@iteo.com>
2022-08-08 14:01:34 +0200

    updated setup.py version

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ django_classifiers = [
 
 setup(
     name=name,
-    version="0.4.2",
+    version="0.4.3",
     packages=find_packages(),
     install_requires=get_reqiuired_packages(),
     description=description,
