54b7195d45c7c41931400738b10681d01d7b142d
mtyton <mateusz.tyton@iteo.com>
2022-08-08 20:30:56 +0200

    fixed tag in setup.py

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ django_classifiers = [
 
 setup(
     name=name,
-    version="0.4.5",
+    version="0.4.4",
     packages=find_packages(),
     install_requires=get_reqiuired_packages(),
     description=description,
