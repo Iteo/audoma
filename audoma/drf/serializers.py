945479172651f6d4b9ddca37058c98ca5d6634da
mtyton <mateusz.tyton@iteo.com>
2022-08-18 15:46:54 +0200

    PINT-50: fixed many issues

--- audoma/drf/serializers.py
+++ audoma/drf/serializers.py
@@ -130,10 +130,10 @@ class ResultSerializerClassMixin:
 
     @classmethod
     def get_result_serializer_class(
-        cls, many: bool = False
+        cls,
     ) -> Type[serializers.BaseSerializer]:
         if cls._wrap_result_serializer:
-            return result_serializer_class(cls, many=many)
+            return result_serializer_class(cls)
         return cls
 
 
