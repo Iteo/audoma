0e36ae66849664f3f87718e690bf8539c03c496a
mtyton <mateusz.tyton@iteo.com>
2022-11-17 14:03:02 +0100

    Added custom SerializerMethodField

--- audoma/drf/fields.py
+++ audoma/drf/fields.py
@@ -5,10 +5,13 @@ and also has defined schema type.
 """
 
 import sys
+from inspect import isclass
 
 import exrex
 import phonenumbers
 from djmoney.contrib.django_rest_framework import MoneyField
+from drf_spectacular.drainage import set_override
+from drf_spectacular.plumbing import force_instance
 from drf_spectacular.types import OpenApiTypes
 from drf_spectacular.utils import extend_schema_field
 from phonenumber_field import serializerfields
@@ -44,7 +47,6 @@ field_names = [
     "HStoreField",
     "JSONField",
     "ReadOnlyField",
-    "SerializerMethodField",
 ]
 
 
@@ -144,3 +146,58 @@ class ChoiceField(ExampleMixin, fields.ChoiceField):
         else:
             self.original_choices = choices
         super().__init__(choices, **kwargs)
+
+
+class SerializerMethodField(ExampleMixin, fields.Field):
+    def _parse_field(self, field):
+        if isclass(field) and issubclass(field, fields.Field):
+            return force_instance(field)
+        elif isinstance(field, fields.Field):
+            return field
+        else:
+            raise ValueError(
+                f"Incorrect type of field, field \
+                    must be an instance or a subclass of rest_framework.fields.Field.\
+                        Passed value: {field}"
+            )
+
+    def __init__(self, *args, **kwargs) -> None:
+        self.method_name = kwargs.pop("method_name", None)
+        self.field = self._parse_field(kwargs.pop("field", None))
+        self.is_writable = kwargs.pop("is_writable", False)
+        if self.is_writable and self.field is None:
+            raise ValueError("Writable SerializerMethodField must have field defined.")
+
+        kwargs["source"] = "*"
+        kwargs["read_only"] = not self.is_writable
+        super().__init__(*args, **kwargs)
+
+    def bind(self, field_name, parent):
+        # The method name defaults to `get_{field_name}`.
+        if self.method_name is None:
+            self.method_name = "get_{field_name}".format(field_name=field_name)
+        super().bind(field_name, parent)
+        if self.field is not None:
+            set_override(self, "field", self.field)
+
+    def to_representation(self, value):
+        method = getattr(self.parent, self.method_name)
+        value = method(value)
+        if not self.field:
+            return value
+        else:
+            return self.field.to_representation(value)
+
+    def get_default(self):
+        default = super().get_default()
+        return default
+
+    def to_internal_value(self, data):
+        if not self.is_writable:
+            raise fields.SkipField
+        return self.field.to_internal_value(data)
+
+    def run_validation(self, data):
+        if not self.is_writable:
+            raise fields.SkipField
+        return self.field.run_validation(data)
