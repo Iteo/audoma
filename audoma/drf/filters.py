ccb5dc5b4dbb4473b0bd2626b94895b02cf42d8d
mtyton <mateusz.tyton@iteo.com>
2023-10-16 08:35:32 +0200

    DocumentedTypedChoiceFilter now accepting just a tuple also

--- audoma/drf/filters.py
+++ audoma/drf/filters.py
@@ -1,4 +1,8 @@
-from typing import NamedTuple
+from typing import (
+    NamedTuple,
+    Tuple,
+    Union,
+)
 
 from django_filters import rest_framework as df_filters
 
@@ -8,17 +12,24 @@ from audoma.plumbing import create_choices_enum_description
 class DocumentedTypedChoiceFilter(df_filters.TypedChoiceFilter):
     """Extended TypedChoiceFilter to generate documentation automatically"""
 
-    def __init__(self, full_choices: NamedTuple, parameter_name: str, **kwargs) -> None:
+    def __init__(
+        self, full_choices: Union[NamedTuple, Tuple], parameter_name: str, **kwargs
+    ) -> None:
+        self.parsed_choices = (
+            full_choices.get_api_choices()
+            if hasattr(full_choices, "get_choices")
+            else full_choices
+        )
         super().__init__(
             coerce=lambda value: full_choices.get_value_by_name(value),
-            choices=full_choices.get_api_choices(),
+            choices=self.parsed_choices,
             **kwargs,
         )
         self.full_choices = full_choices
         self.parameter_name = parameter_name
         self.extra["help_text"] = self.extra.get("help_text", "{choices}").format(
             choices=create_choices_enum_description(
-                full_choices.get_api_choices(), self.field_name
+                self.parsed_choices, self.field_name
             )
         )
-        self.extra["choices"] = full_choices.get_api_choices()
+        self.extra["choices"] = self.parsed_choices
